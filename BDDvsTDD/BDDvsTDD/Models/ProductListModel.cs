6965b311d4394df5e06fb74053d69840f34f4d48
BruceW <bruce.weber@t2informatik.de>
2022-04-06 12:42:38 +0200

    #8 implemented add entries and get entry

--- BDDvsTDD/BDDvsTDD/Models/ProductListModel.cs
+++ BDDvsTDD/BDDvsTDD/Models/ProductListModel.cs
@@ -11,9 +11,15 @@ namespace BDDvsTDD
         public ProductListModel() { entries = new List<Product>(0); }
         private List<Product> entries;
         public Product[] getEntries() { return entries.ToArray(); }
-        public void addEntry(string name, float price, int amount) { }
+        public void addEntry(string name, float price, int amount) 
+        {
+            entries.Add(new Product(name, price, amount));
+        }
         public void removeEntry(string name) { }
         public void editEntry(string name) { }
-        public Product getEntry(string name) { return new Product("", 0, 0); }
+        public Product getEntry(string name) 
+        { 
+            return entries.FirstOrDefault(product => product.name == name);
+        }
     }
 }
